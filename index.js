75e05ef40e2808dda0fabc27a54bb40862a7c484
Dmitriy Seryogin <dmitriyseryogin0@gmail.com>
2026-01-17 18:09:12 +0200

    added notifying about power outages schedule from message

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import http from 'http';
 import 'dotenv/config';
-import {readFile, writeFile} from 'fs/promises';
 
 puppeteer.use(StealthPlugin());
 
-const URL = 'https://www.dtek-kem.com.ua/ua/shutdowns';
 const GROUP = 'GPV6.2'; // your group here
 
 const server = http.createServer((req, res) => {
@@ -15,26 +13,16 @@ const server = http.createServer((req, res) => {
 });
 server.listen(process.env.PORT || 3000);
 
+let currentScheduleFromMessage = null;
+let currentScheduleFromTable = null;
+
 async function getRawHtml() {
-    const browser = await puppeteer.launch({
-        headless: 'new',
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-gpu',
-            '--single-process',
-            '--no-zygote',
-        ],
-        ignoreHTTPSErrors: true,
-    });
+    const URL = 'https://www.dtek-kem.com.ua/ua/shutdowns';
+
+    const browser = await puppeteer.launch();
 
     try {
         const page = await browser.newPage();
-
-        await page.setUserAgent(
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
-        );
 
         console.log('Loading page...');
         await page.goto(URL, {waitUntil: 'networkidle0', timeout: 60000});
@@ -48,6 +36,54 @@ async function getRawHtml() {
     } finally {
         await browser.close();
     }
+}
+
+async function getDataFromTextMessage() {
+    const URL = 'https://www.dtek-kem.com.ua/ua/shutdowns';
+
+    const browser = await puppeteer.launch();
+
+    const page = await browser.newPage();
+
+    console.log('Loading json data...');
+    const res = await page.goto(URL);
+
+    const requestHeaders = res.request().headers();
+    const csrfToken = await page.$eval('meta[name=csrf-token]', el => el.content);
+
+    const json = await page.evaluate(async (csrfToken, cookie) => {
+        const body = new URLSearchParams();
+        body.append('method', 'getHomeNum');
+        body.append('data[0][name]', 'street');
+        body.append('data[0][value]', 'вул. Клавдіївська');
+        body.append('data[1][name]', 'updateFact');
+        body.append('data[1][value]', new Date().toLocaleDateString('uk-UA'));
+
+        document.cookie = 'Domain=dtek-kem.com.ua;\n' + cookie;
+
+        const response = await fetch('https://www.dtek-kem.com.ua/ua/ajax', {
+            method: 'POST',
+            headers: {
+                'accept': 'application/json, text/javascript, */*; q=0.01',
+                'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'x-requested-with': 'XMLHttpRequest',
+                'x-csrf-token': csrfToken,
+            },
+            body: body.toString(),
+        });
+
+        const text = await response.text();
+
+        try {
+            return JSON.parse(text);
+        } catch {
+            return {error: 'Non-JSON response', raw: text};
+        }
+    }, csrfToken, requestHeaders.cookie);
+
+    await browser.close();
+
+    return json;
 }
 
 async function parseScheduleFromHtml(html) {
@@ -74,10 +110,10 @@ async function parseScheduleFromHtml(html) {
 
 function formatScheduleToMessage(parsed) {
     let message = '';
-    const currentSchedule = {};
+    const newSchedule = {};
 
     Object.keys(parsed.data).forEach((dateTimestamp, i) => {
-        currentSchedule[dateTimestamp] = [];
+        newSchedule[dateTimestamp] = [];
         const date = new Date(Number(dateTimestamp) * 1000);
         const uaDate = new Intl.DateTimeFormat('uk-UA', {
             day: 'numeric',
@@ -125,7 +161,7 @@ function formatScheduleToMessage(parsed) {
                             message += hour;
                             currentPeriod += hour;
                         }
-                        currentSchedule[dateTimestamp].push(currentPeriod);
+                        newSchedule[dateTimestamp].push(currentPeriod);
                         assigningNewPeriod = false;
                         isStartOfPeriodInitialized = false;
                         currentPeriod = '';
@@ -141,7 +177,7 @@ function formatScheduleToMessage(parsed) {
                         message += hour;
                         currentPeriod += hour;
                     }
-                    currentSchedule[dateTimestamp].push(currentPeriod);
+                    newSchedule[dateTimestamp].push(currentPeriod);
                     assigningNewPeriod = false;
                     isStartOfPeriodInitialized = false;
                     currentPeriod = '';
@@ -154,7 +190,9 @@ function formatScheduleToMessage(parsed) {
         });
     });
 
-    return {message, currentSchedule};
+    message = 'Из таблицы:\n' + message;
+
+    return {message, newSchedule};
 }
 
 async function sendTelegramNotification(text) {
@@ -167,7 +205,7 @@ async function sendTelegramNotification(text) {
     }
 
     try {
-        const res = await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+        await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -177,7 +215,7 @@ async function sendTelegramNotification(text) {
             }),
         });
 
-        console.log(`Notification sent at ${new Date().toISOString()}`);
+        console.log(`Notification sent`);
     } catch (err) {
         console.error('Telegram send failed:', err);
     }
@@ -190,25 +228,52 @@ async function main() {
         const html = await getRawHtml();
         const parsed = await parseScheduleFromHtml(html);
 
-        const {message, currentSchedule} = formatScheduleToMessage(parsed);
+        let {message, newSchedule} = formatScheduleToMessage(parsed);
 
-        let stored = null;
-        try {
-            const content = await readFile('schedule.json', 'utf-8');
-            stored = JSON.parse(content);
-        } catch {
-            // first run
+        let shouldNotify = !currentScheduleFromTable;
+
+        if (currentScheduleFromTable) {
+            shouldNotify = JSON.stringify(currentScheduleFromTable) !== JSON.stringify(currentScheduleFromTable);
+
+            if (shouldNotify) {
+                currentScheduleFromTable = newSchedule;
+            } else {
+                message = '';
+            }
+        } else {
+            currentScheduleFromTable = newSchedule;
+            shouldNotify = true;
         }
 
-        let shouldNotify = !stored;
+        const json = await getDataFromTextMessage();
 
-        if (stored) {
-            shouldNotify = JSON.stringify(stored) !== JSON.stringify(currentSchedule);
+        if (!json.error) {
+            const myHouseData = json.data["23/15"];
+
+            const dates = {
+                sub_type: myHouseData.sub_type,
+                start_date: myHouseData.start_date,
+                end_date: myHouseData.end_date
+            };
+
+            const additionalMessage = `\n\nИз сообщения:\n${myHouseData.sub_type}\n${dates.start_date} - ${dates.end_date}`;
+
+            if (currentScheduleFromMessage) {
+                if (JSON.stringify(currentScheduleFromMessage) !== JSON.stringify(dates)) {
+                    currentScheduleFromMessage = dates;
+                    message += `\n${additionalMessage}`;
+                    shouldNotify = true;
+                }
+            } else {
+                currentScheduleFromMessage = dates;
+                message += additionalMessage;
+                shouldNotify = true;
+            }
         }
 
         if (shouldNotify) {
-            console.log('Schedule changed → sending notification');
-            await writeFile('schedule.json', JSON.stringify(currentSchedule, null, 2));
+            console.log('Sending notification');
+
             if (message.trim()) {
                 await sendTelegramNotification(message);
             }
@@ -220,7 +285,6 @@ async function main() {
     }
 }
 
-// Keep alive
 const keepAlive = () => {
     fetch('https://power-outage-notifier-976l.onrender.com').catch(() => {
     });
